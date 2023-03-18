6a510268d1d20fecea8cf00d005cc2aa19086f7e
Maxim Bondarenko <maxim8bondarenko@gmail.com>
2023-03-19 03:44:57 +1100

    Initial Commit

---
+++ TrafficLightApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLightApp
+//
+//  Created by Максим Бондаренко on 19.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
